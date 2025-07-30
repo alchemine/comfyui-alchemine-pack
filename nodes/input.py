623cefa3ce114a55666d4e00920e8ac78f2e388f
alchemine <djyoon0223@gmail.com>
2025-07-30 23:08:29 +0900

    feat: change inputs of WidthHeight node

--- nodes/input.py
+++ nodes/input.py
@@ -20,19 +20,10 @@ class WidthHeight(BasePrompt):
 
     INPUT_TYPES = lambda: {
         "required": {
-            "aspect_ratio": (
-                [
-                    "832 x 480",
-                    "640 x 480",
-                    "480 x 640",
-                    "480 x 832",
-                    "512 x 512",
-                    "640 x 640",
-                ],
-                {"default": "480 x 640"},
-            ),
+            "width": ("INT", {"default": 480, "min": 1}),
+            "height": ("INT", {"default": 640, "min": 1}),
+            "swap": ("BOOLEAN", {"default": False}),
             "scale": ("FLOAT", {"default": 1.0, "min": 1.0}),
-            "validate_by_16": ("BOOLEAN", {"default": True}),
         }
     }
     RETURN_TYPES = ("INT", "INT")
@@ -42,28 +33,12 @@ class WidthHeight(BasePrompt):
 
     @classmethod
     def execute(
-        cls,
-        aspect_ratio: str = "480 x 640",
-        scale: float = 1.0,
-        validate_by_16: bool = True,
+        cls, width: int = 480, height: int = 640, swap: bool = False, scale: float = 1.0
     ) -> tuple[int, int]:
-        width, height = map(int, aspect_ratio.split("x"))
-        return int(width * scale), int(height * scale)
-
-    @classmethod
-    def VALIDATE_INPUTS(
-        cls,
-        aspect_ratio: str = "480 x 640",
-        scale: float = 1.0,
-        validate_by_16: bool = True,
-    ) -> bool:
-        # Check if width and height are divisible by 16
-        width, height = map(int, aspect_ratio.split("x"))
         width, height = int(width * scale), int(height * scale)
-        if validate_by_16:
-            return width % 16 == 0 and height % 16 == 0
-        else:
-            return True
+        if swap:
+            width, height = height, width
+        return width, height
 
 
 if __name__ == "__main__":
